dd265a13aaf71fc3bdf45253e867e2063bb8113b
srajsonu <43319626+srajsonu@users.noreply.github.com>
2019-01-06 21:15:56 +0530

    Adding CoreML

--- seaFood/ViewController.swift
+++ seaFood/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
     }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[.originalImage] as? UIImage {
+         imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {fatalError("Could not convert in CIImage")}
+            
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {fatalError("Loading coreML  Model failed")}
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {fatalError("Unable to retrieve info")}
+            print(results)
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+        
+    }
+    
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
 }
 
